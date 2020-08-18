59a06e30c9db6821c067be583a42b33b288c6163
liangxiuqin <liangxiuqin@mininglamp.com>
2020-08-18 17:22:01 +0800

    update decision-tree

--- src/main/java/xiuqin/ml/decisiontree/DecisionTree.java
+++ src/main/java/xiuqin/ml/decisiontree/DecisionTree.java
@@ -6,6 +6,7 @@ import org.nd4j.linalg.indexing.BooleanIndexing;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.nd4j.linalg.indexing.conditions.Conditions;
 import org.nd4j.linalg.indexing.conditions.EqualsCondition;
+import org.nd4j.linalg.ops.transforms.Transforms;
 import xiuqin.ml.ModelBase;
 
 public class DecisionTree extends ModelBase {
@@ -68,19 +69,22 @@ public class DecisionTree extends ModelBase {
     private double calcEntropy(INDArray labelArr) {
         double HD = 0;
 
+        if (labelArr.isEmpty()) {
+            return HD;
+        }
+
         for (int i = 0; i < LABEL_COUNT; i++) {
             int count = labelArr.scan(new EqualsCondition(i)).intValue();
             double prob = 1.0 * count / labelArr.columns();
 
-            HD += -1.0 * prob * Math.log(prob);
+            HD += -1.0 * prob * (Math.log(prob) / Math.log(2));     //log2(prob)=Math.log(prob) / Math.log(2)
         }
 
-        double hao = labelArr.entropyNumber().doubleValue();
         return HD;
     }
 
     //conditional entropy
-    private double calcCondEntropy(INDArray xiFeature) {
+    private double calcCondEntropy(INDArray xiFeature, INDArray sourceLabelArr) {
         double HDA = 0;
 
         for (int i = 0; i < featureValues.length; i++) {
@@ -89,19 +93,19 @@ public class DecisionTree extends ModelBase {
 
             //count of the value equal one featureValues
             int count = xiFeature.scan(new EqualsCondition(value)).intValue();
+            if (count == 0) continue;  //if count==0
             INDArray labelArr = Nd4j.create(count);
 
             int index = 0;
             for (int j = 0; j < xiFeature.columns(); j++) {
                 //if the value equal one featureValues
                 if (xiFeature.getInt(j) == value) {
-                    labelArr.putScalar(index, this.trainLabelArr.getInt(j));   //get the label value at the specified index
+                    labelArr.putScalar(index, sourceLabelArr.getInt(j));   //get the label value at the specified index
                     index++;   //if put one value, then accumulator
                 }
             }
 
-            count = xiFeature.scan(new EqualsCondition(value)).intValue();
-            double prob = 1.0 * count / xiFeature.columns();  //H(D)
+            double prob = 1.0 * count / xiFeature.columns();  //P(Di)
             HDA += prob * calcEntropy(labelArr);  //H(D|A)
         }
 
@@ -109,7 +113,7 @@ public class DecisionTree extends ModelBase {
     }
 
     //information gain
-    private double[] calcInfoGain() {
+    private double[] calcInfoGain(INDArray dataArr, INDArray labelArr) {
         //init max information gain var
         double gain = -1.0;
 
@@ -117,12 +121,12 @@ public class DecisionTree extends ModelBase {
         int index = -1;
 
         //get empirical entropy
-        double entropy = calcEntropy(this.trainLabelArr);
+        double entropy = calcEntropy(labelArr);
 
         //foreach every feature
-        for (int i = 0; i < FEATURES; i++) {
-            INDArray feautre_xi = this.trainDataArr.getColumn(i);
-            double temp = entropy - calcCondEntropy(feautre_xi);
+        for (int i = 0; i < dataArr.columns(); i++) {
+            INDArray feature_xi = dataArr.getColumn(i);
+            double temp = entropy - calcCondEntropy(feature_xi, labelArr);
             if (temp > gain) {
                 gain = temp;
                 index = i;
@@ -132,27 +136,32 @@ public class DecisionTree extends ModelBase {
         return new double[]{index, gain};  //return the tuple of index and gain
     }
 
-    private INDArray[] getSubDataArr(int deleteIndex, int value) {
-        int count = this.trainDataArr.getColumns(deleteIndex).scan(new EqualsCondition(value)).intValue();
-        INDArray dataArr = Nd4j.create(count, FEATURES - 1);
+    private INDArray[] getSubDataArr(INDArray sourceDataArr, INDArray sourceLabelArr, int deleteIndex, int value) {
+        int count = sourceDataArr.getColumns(deleteIndex).scan(new EqualsCondition(value)).intValue();
+        INDArray dataArr = Nd4j.create(count, sourceDataArr.columns() - 1);   //do not care columns() equal 0 and 1
         INDArray labelArr = Nd4j.create(count);
 
-        int index = 0;
-        for (int i = 0; i < this.trainDataArr.rows(); i++) {
-            if (this.trainDataArr.getInt(i, deleteIndex) == value) {
-                dataArr.put(index,
-                        Nd4j.hstack(this.trainDataArr.get(NDArrayIndex.interval(0, deleteIndex)),
-                                this.trainDataArr.get(NDArrayIndex.interval(deleteIndex, this.trainDataArr.columns()))));
+        if (count > 0) {
+            int index = 0;
+            for (int i = 0; i < sourceDataArr.rows(); i++) {
+                if (sourceDataArr.getInt(i, deleteIndex) == value) {
+                    INDArray row = sourceDataArr.getRow(i);
+                    INDArray left = row.get(NDArrayIndex.interval(0, deleteIndex));
+                    INDArray right = row.get(NDArrayIndex.interval(deleteIndex + 1, row.columns()));
+                    row = Nd4j.hstack(left, right);  //dimension is 1
+                    dataArr.putRow(index, row);
 
-                labelArr.putScalar(index, this.trainLabelArr.getInt(i));
+                    labelArr.putScalar(index, sourceLabelArr.getInt(i));
 
-                index++;
+                    index++;
+                }
             }
         }
 
         return new INDArray[]{dataArr, labelArr};  //return the tuple of sub_data and sub_label
     }
 
+    //ID3 algo
     class DTree {
         int index = -1;   //init index is -1
         int label;
@@ -183,26 +192,28 @@ public class DecisionTree extends ModelBase {
         }
     }
 
+    //use ID3 algo to create DTree
     private DTree createTree(INDArray dataArr, INDArray labelArr) {
         //set epsilon
         double epsilon = 0.1;
 
         System.out.println(String.format("start a node: feature_count=%d,sample_count=%d", dataArr.columns(), labelArr.columns()));
 
+        //if one feature use dataArr.rows()==1
+        if (dataArr.isEmpty() || dataArr.rows() == 1) {
+            int label = getMajorClass(labelArr);
+            return new DTree(label);
+        }
+
+        //if the label are the same
         int label = labelArr.getInt(0);
         int count = labelArr.scan(new EqualsCondition(label)).intValue();  //get the count of values equal labelArr[0]
         if (count == labelArr.columns()) {
             return new DTree(label);
         }
 
-        //if one feature
-        if (dataArr.columns() == 1) {
-            label = getMajorClass(labelArr);
-            return new DTree(label);
-        }
-
         //get best feature
-        double[] ag_epsilon = calcInfoGain();
+        double[] ag_epsilon = calcInfoGain(dataArr, labelArr);
 
         //if < init epsilon
         if (ag_epsilon[1] < epsilon) {
@@ -212,10 +223,10 @@ public class DecisionTree extends ModelBase {
 
         int ag = (int) ag_epsilon[0];
 
-        INDArray[] data_label = getSubDataArr(ag, 0);   //value is 0
+        INDArray[] data_label = getSubDataArr(dataArr, labelArr, ag, 0);   //value is 0
         DTree left = createTree(data_label[0], data_label[1]);
 
-        data_label = getSubDataArr(ag, 1);  //value is 1
+        data_label = getSubDataArr(dataArr, labelArr, ag, 1);  //value is 1
         DTree right = createTree(data_label[0], data_label[1]);
 
         return new DTree(ag, left, right);
@@ -228,7 +239,7 @@ public class DecisionTree extends ModelBase {
     private double modelTest(DTree dTree) {
         int errorCount = 0;
 
-        int testCount = 200;    //this.testDataArr.rows();
+        int testCount = this.testDataArr.rows();
         for (int i = 0; i < testCount; i++) {
             INDArray each = this.testDataArr.getRow(i);
             long label = predict(each, dTree);   //get predict
@@ -237,7 +248,7 @@ public class DecisionTree extends ModelBase {
                 errorCount += 1;
             }
 
-            if (i % 10 == 0) {
+            if (i % 100 == 0) {
                 System.out.println("testing:" + i);
             }
         }
