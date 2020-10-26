ca86fde8ebafe14d76231eeb3c0130f346469feb
liangxiuqin <liangxiuqin@mininglamp.com>
2020-10-26 19:12:08 +0800

    add pca4svd

--- src/main/java/xiuqin/ml/ext/PCA4EVD.java
+++ src/main/java/xiuqin/ml/ext/PCA4EVD.java
@@ -1,20 +1,21 @@
 package xiuqin.ml.ext;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dimensionalityreduction.PCA;
 import org.nd4j.linalg.eigen.Eigen;
 import org.nd4j.linalg.factory.Nd4j;
 
 //基于特征值分解实现PCA算法
 public class PCA4EVD {
     public static void main(String[] args) {
-        PCA4EVD pca=new PCA4EVD();
+        PCA4EVD pca = new PCA4EVD();
         INDArray hao = Nd4j.create(new float[]{-1, 1, -2, -1, -3, -2, 1, 1, 2, 1, 3, 2}, new int[]{6, 2});
-        //System.out.println(hao);
+        System.out.println(hao);
 
-        //PCA h=new PCA(hao);
-        //System.out.println(h.reducedBasis(1));
+        INDArray temp = hao.dup();
+        System.out.println(PCA.pca(temp, 1, true));
 
-        INDArray result=pca.pca(hao);
+        INDArray result = pca.pca(hao);
         System.out.println(result);
     }
 
@@ -23,10 +24,10 @@ public class PCA4EVD {
         INDArray mean = X.mean(0);  //以行向量作平均
 
         //2、正则化处理X元素值
-        INDArray norm=X.sub(mean);
+        INDArray norm = X.sub(mean);
 
-        //3、求散度矩阵:X*X^T
-        INDArray scatter = norm.transpose().mmul(norm);
+        //3、求散度矩阵:X^T*X
+        INDArray scatter = norm.transpose().mmul(norm);   //covarianceMatrix(X)[0];
 
         //4、Calculate the eigenvectors and eigenvalues
         assert scatter.rows() == scatter.columns();
@@ -34,24 +35,25 @@ public class PCA4EVD {
         INDArray eigVal = Eigen.symmetricGeneralizedEigenvalues(eigVec, scatter, true);
 
         //5、sort eig_vec based on eig_val from highest to lowest
-        int maxIndex=Nd4j.argMax(eigVal).getInt(0);
+        int maxIndex = Nd4j.argMax(eigVal).getInt(0);
 
         //6、select the top k eig_vec
         INDArray feature = eigVec.getRow(maxIndex);  //降成1维
 
         //7、生成一个新data
-        return X.mmul(feature.reshape(X.columns()));
+        return X.mmul(feature.reshape(X.columns(),1));
     }
 
-    /*public static INDArray[] covarianceMatrix(INDArray in) {
+    public static INDArray[] covarianceMatrix2(INDArray in) {
         long dlength = in.rows();
         long vlength = in.columns();
 
         INDArray sum = Nd4j.create(vlength);
         INDArray product = Nd4j.create(vlength, vlength);
 
-        for (int i = 0; i < vlength; i++)
+        for (int i = 0; i < vlength; i++) {
             sum.getColumn(i).assign(in.getColumn(i).sumNumber().doubleValue() / dlength);
+        }
 
         for (int i = 0; i < dlength; i++) {
             INDArray dx1 = in.getRow(i).sub(sum);
@@ -59,16 +61,16 @@ public class PCA4EVD {
         }
         product.divi(dlength);
         return new INDArray[]{product, sum};
-    }*/
+    }
 
     public static INDArray[] covarianceMatrix(INDArray in) {
-        long dlength = (long)in.rows();
-        long vlength = (long)in.columns();
+        long dlength = (long) in.rows();
+        long vlength = (long) in.columns();
         INDArray product = Nd4j.create(new long[]{vlength, vlength});
         INDArray sum = in.sum(new int[]{0}).divi(dlength);   //变成均值
 
-        for(int i = 0; (long)i < dlength; ++i) {
-            INDArray dx1 = in.getRow((long)i).sub(sum);
+        for (int i = 0; (long) i < dlength; ++i) {
+            INDArray dx1 = in.getRow((long) i).sub(sum);
             product.addi(dx1.reshape(vlength, 1L).mmul(dx1.reshape(1L, vlength)));
         }
 
---
+++ src/main/java/xiuqin/ml/ext/PCA4SVD.java
@@ -0,0 +1,58 @@
+package xiuqin.ml.ext;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dimensionalityreduction.PCA;
+import org.nd4j.linalg.factory.Nd4j;
+
+//use svd for pca
+public class PCA4SVD {
+    public static void main(String[] args) {
+        PCA4SVD pca = new PCA4SVD();
+        INDArray hao = Nd4j.create(new float[]{-1, 1, -2, -1, -3, -2, 1, 1, 2, 1, 3, 2}, new int[]{6, 2});
+        System.out.println(hao);
+
+        //PCA h = new PCA(hao);
+        //System.out.println(h);
+
+        INDArray result = pca.pca(hao, 1, true);
+        System.out.println(result);
+    }
+
+    public INDArray pca(INDArray A, int nDims, boolean normalize) {
+        INDArray factor = pca_factor(A, nDims, normalize);
+        return A.mmul(factor);
+    }
+
+    //use svd directly without eigenvalue decomposition
+    private INDArray pca_factor(INDArray A, int nDims, boolean normalize) {
+
+        if (normalize) {
+            // Normalize to mean 0 for each feature ( each column has 0 mean )
+            INDArray mean = A.mean(0);
+            A.subiRowVector(mean);
+        }
+
+        long m = A.rows();
+        long n = A.columns();
+
+        // The prepare SVD results, we'll decomp A to UxSxV'
+        INDArray s = Nd4j.create(A.dataType(), m < n ? m : n);
+
+        INDArray VT = Nd4j.create(A.dataType(), new long[]{n, n}, 'f');
+
+        // Note - we don't care about U
+        Nd4j.getBlasWrapper().lapack().gesvd(A, s, null, VT);
+
+        // for comparison k & nDims are the equivalent values in both methods implementing PCA
+
+        // So now let's rip out the appropriate number of left singular vectors from
+        // the V output (note we pulls rows since VT is a transpose of V)
+        INDArray V = VT.transpose();  //use V
+        INDArray factor = Nd4j.create(A.dataType(), new long[]{n, nDims}, 'f');
+        for (int i = 0; i < nDims; i++) {
+            factor.putColumn(i, V.getColumn(i));
+        }
+
+        return factor;
+    }
+}
